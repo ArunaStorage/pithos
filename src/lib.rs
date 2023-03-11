b2ccad7801b6f11ac0c825168c0e4a5548407857
Sebastian Beyvers <sb@pus.de>
2023-03-11 23:01:59 +0100

    feat: Added test from Vec

--- src/lib.rs
+++ src/lib.rs
@@ -45,4 +45,37 @@ mod tests {
             .await
             .unwrap()
     }
+
+    #[tokio::test]
+    async fn test_with_vec() {
+        let file = b"This is a very very important test".to_vec();
+        let mut file2 = Vec::new();
+
+        // Create a new ArunaReadWriter
+        // Add transformer in reverse order -> from "last" to first
+        // input -> 1 -> 2 -> 3 -> output
+        // .add(3).add(2).add(1)println!("{}", self.internal_buf.len());
+        ArunaReadWriter::new(file.as_ref(), &mut file2)
+            .add_transformer(ZstdDec::new()) // Double compression because we can
+            .add_transformer(ZstdDec::new()) // Double compression because we can
+            .add_transformer(
+                ChaCha20Dec::new(b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
+            )
+            .add_transformer(
+                ChaCha20Dec::new(b"99wj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
+            )
+            .add_transformer(
+                ChaCha20Enc::new(false, b"99wj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
+            )
+            .add_transformer(
+                ChaCha20Enc::new(false, b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
+            ) // Tripple compression because we can
+            .add_transformer(ZstdEnc::new(2, false)) // Double compression because we can
+            .add_transformer(ZstdEnc::new(1, false))
+            .process()
+            .await
+            .unwrap();
+
+        assert_eq!(file, file2)
+    }
 }
