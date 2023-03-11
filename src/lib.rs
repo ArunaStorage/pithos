2f56ffebe5b92f2485cac47c382d4b950d0d0bdb
Sebastian Beyvers <sb@pus.de>
2023-03-11 22:55:56 +0100

    feat: Added tests for double compression and encryption

--- src/lib.rs
+++ src/lib.rs
@@ -25,15 +25,21 @@ mod tests {
         // input -> 1 -> 2 -> 3 -> output
         // .add(3).add(2).add(1)println!("{}", self.internal_buf.len());
         ArunaReadWriter::new(file, file2)
-            //.add_transformer(ZstdDec::new()) // Double compression because we can
+            .add_transformer(ZstdDec::new()) // Double compression because we can
             .add_transformer(ZstdDec::new()) // Double compression because we can
             .add_transformer(
                 ChaCha20Dec::new(b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
             )
             .add_transformer(
+                ChaCha20Dec::new(b"99wj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
+            )
+            .add_transformer(
+                ChaCha20Enc::new(false, b"99wj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
+            )
+            .add_transformer(
                 ChaCha20Enc::new(false, b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea".to_vec()).unwrap(),
             ) // Tripple compression because we can
-            //.add_transformer(ZstdEnc::new(2, false)) // Double compression because we can
+            .add_transformer(ZstdEnc::new(2, false)) // Double compression because we can
             .add_transformer(ZstdEnc::new(1, false))
             .process()
             .await
