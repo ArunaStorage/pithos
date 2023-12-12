2c2a4ada094fcca6aea33b859eb6db9087a01a47
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-12-12 13:08:12 +0100

    fix: Incorrect buffer discard in Filter transformer

--- src/transformers/filter.rs
+++ src/transformers/filter.rs
@@ -29,21 +29,27 @@ impl Transformer for Filter {
     async fn process_bytes(&mut self, buf: &mut bytes::BytesMut, _: bool, _: bool) -> Result<bool> {
         self.captured_buf_len = buf.len();
         self.advanced_by = 0;
+
+        // If bytes are present in the buffer
         if !buf.is_empty() {
+            // If counter + incoming bytes are larger than lower limit
+            //   -> Advance buffer to lower limit
             if ((self.counter + self.captured_buf_len) as u64) > self.filter.from {
-                if self.counter > self.filter.from as usize {
-                    buf.clear();
-                } else {
-                    self.advanced_by = self.filter.from as usize - self.counter;
-                    buf.advance(self.advanced_by);
-                }
+                self.advanced_by = self.filter.from as usize - self.counter;
+                buf.advance(self.advanced_by);
             } else {
+                // If counter + incoming bytes are smaller than lower limit
+                //   -> discard buffer
                 buf.clear();
             }
 
             if self.counter as u64 > self.filter.to {
+                // If counter is larger than upper limit
+                //   -> discard buffer
                 buf.clear();
             } else if self.counter as u64 + self.captured_buf_len as u64 > self.filter.to {
+                // If counter + incoming bytes is larger than upper limit
+                //   -> truncate buffer to upper limit
                 buf.truncate(self.filter.to as usize - self.advanced_by - self.counter);
             }
         }
